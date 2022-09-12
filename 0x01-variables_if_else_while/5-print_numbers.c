df4e393560649974116a1c3f8e18838a07fb8262
ericnick101 <ericovisky@gmail.com>
2022-09-12 16:46:25 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,27 +10,11 @@
 
 int main(void)
 {
-	int p, q;
+	int i;
 
-	for (p = 0; p <= 98; p++)
-	{
-		for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar(' ');
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-
-			if (p == 98 && q == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 
 	return (0);
 }

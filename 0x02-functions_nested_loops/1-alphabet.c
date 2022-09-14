cb7bc4c8abdce0692ddd1af5b37a3d42a195819e
ericnick101 <ericovisky@gmail.com>
2022-09-14 02:48:38 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,17 +2,16 @@
 
 /**
  * print_alphabet - function to print abc
- * 
+ *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
-	
+	for (c = 'a'; c <= 'z'; c++)	
         {
 		_putchar(c);
 	}
-	_putchar('\n');
+_putchar('\n');
 }

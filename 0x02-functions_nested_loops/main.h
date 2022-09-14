f49dbff98fce7bed6c354bcd2acaa5eba3ec56ee
ericnick101 <ericovisky@gmail.com>
2022-09-14 02:32:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,6 +15,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
 
-#end
-
-^ C
+#endif

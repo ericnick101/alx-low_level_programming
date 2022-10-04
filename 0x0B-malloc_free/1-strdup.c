f036e6cd16fdc7c1601201ba40518d516eef5bcb
ericnick101 <ericovisky@gmail.com>
2022-10-04 10:48:11 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,24 +8,9 @@
  */
 char *_strdup(char *str)
 {
-	char *strnew = NULL;
-	unsigned int i;
-	int n;
+	char *nstr;
+	usigned int len, i;
 
-	if (str == NULL)
-		return (NULL);
-
-	for (n = 0; str[n] != '\0'; n++);
-
-	strnew = (char *)malloc(n + 1 * sizeof(char));
-	if (strnew != NULL)
-	{
-		for (i = 0; str[i] != '\0'; i++)
-			strnew[i] = str[i];
-	} else
-	{
-	return (NULL);
-	}
-strnew[i] = '\0';
-return (strnew);
+	/* check is str is null */
+	if (str == NULL);
 }
